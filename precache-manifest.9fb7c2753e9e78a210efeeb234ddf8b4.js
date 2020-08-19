a484863d30362ea5f07af83c1a5084c02b0a4a9a
Ivan Tilikin <tilvan@ya.ru>
2020-08-19 13:09:30 +0300

    Updates

---
+++ precache-manifest.9fb7c2753e9e78a210efeeb234ddf8b4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5e5d67483e72018b09cc7db75f1f7fe",
+    "url": "/static/media/bg-1.a5e5d674.png"
+  },
+  {
+    "revision": "fd80866f888ceb3b4a8d",
+    "url": "/static/css/main.99ae6275.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "51f453580a909664e989",
+    "url": "/static/js/2.05457c13.chunk.js"
+  },
+  {
+    "revision": "ed8470b81980abea009be4a60bad3b9c",
+    "url": "/static/media/arrow-down.ed8470b8.svg"
+  },
+  {
+    "revision": "db6b23f8ce8bac9de66ae19c00417d0e",
+    "url": "/static/media/logo.db6b23f8.svg"
+  },
+  {
+    "revision": "2ea4d809ecd5526fc8019e836f8ecd84",
+    "url": "/static/media/bg-2.2ea4d809.png"
+  },
+  {
+    "revision": "fd80866f888ceb3b4a8d",
+    "url": "/static/js/main.962d3cfc.chunk.js"
+  },
+  {
+    "revision": "e3104c0f99d406edd6cdf9e423dc82ac",
+    "url": "/static/media/bg-2.e3104c0f.png"
+  },
+  {
+    "revision": "870cb7f8bafdd7e1936a233c0162d039",
+    "url": "/static/media/bg-1.870cb7f8.png"
+  },
+  {
+    "revision": "3e077e7554ba97a40e3de3ce041f966b",
+    "url": "/static/media/icon.3e077e75.svg"
+  },
+  {
+    "revision": "5e4863e678205ea8263942eebda3121f",
+    "url": "/static/media/g.5e4863e6.svg"
+  },
+  {
+    "revision": "1136999aaeb1eff33ca2ded177263f4f",
+    "url": "/static/media/c.1136999a.svg"
+  },
+  {
+    "revision": "708f01fb286186cad0fee10f946802b1",
+    "url": "/index.html"
+  }
+];
